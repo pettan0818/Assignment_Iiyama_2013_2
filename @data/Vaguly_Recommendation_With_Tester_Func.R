896a4fa7ce5e199600d43a4f5862536f357317d5
pettan0818 <pettan0818+git@gmail.com>
2014-01-27 09:56:35 +0900

    [A1] test func is now under const.

--- @data/Vaguly_Recommendation_With_Tester_Func.R
+++ @data/Vaguly_Recommendation_With_Tester_Func.R
@@ -7,13 +7,14 @@
 # Data_Reader
 # 使う列数さえ指定してくれれば、自動で、名前もつけて返します。
 data_reader = function(use_column){
-	read.csv("udata+aorder_labeled_work.csv", header = T, fileEncoding = 'shift-jis')
+	data = read.csv("udata+aorder_labeled_work.csv", header = T, fileEncoding = 'shift-jis')
 	
 	data = data[,use_column]
 	colnames(data) = c("sex","age","pref_u15","pref_now","equivalent","shrimp","conger_eel","tuna","squid","sea_urchin","salmon_roe","egg","fatty flesh","tuna_roll","cucumber")
 	
 	return(data)
 }
+
 # セッション判定(返り値:(BOOL)Interactive = F, Non-Interactive = T)
 # 引数は、各入力データ(sex, age, pref_u15, pref_now)。これらがすべてNAの時スクリプトセッションだと判定する。
 analyzer_session = function(a, b, c, d){	
@@ -45,10 +46,40 @@ k_means = function(original, input_data, cluster = 8){
 	
 	return(same_cluster_users)
 }
+
+# ホールドアウト検定用の関数
+# tester(フラグ, オリジナルデータ, サンプリング個数)
+# デフォルトでは、サンプリング個数は、1割。
+# 返り値について、
+tester = function(with_test_flag, data, sampler = nrow(data)/10){
+	data_origin = data
+	sampled_row = sample(nrow(data),500)
+	data_sampled = data[sampled_row,]
+	data = data[-sampled_row,]
+}
 ##############################################################
 source("../local.r",encoding='utf-8')
 
 # 順位化データの読みこみ
 use_column = c(3, 4, 6, 9, 12, 13:22)
 data = data_reader(use_column)
+
+
+# test Session
+with_test_flag = as.integer(readline(prompt = "[Test Flag] Do you want to suggest with test?(Yes = 1, No = 0)> "))
+if(is.na(with_test_flag)) with_test_flag = 0
+
+# If this Session is non-interactive session, I'll use arguments.
+input_data["sex"] = as.integer(readline(prompt = "Please, Enter your Sex...(M = 0, F = 1)> "))
+
+input_data["age"] = as.integer(readline(prompt = "Please, Enter your Age...(-20 = 0, 20- = 1, 30- = 2, 40- = 3, 50- = 4, 60- = 5)> "))
+
+input_data["pref_u15"] = as.integer(readline(prompt = "Please Enter where had you lived in? (Answer in Pref Number)> "))
+
+input_data["pref_now"] = as.integer(readline(prompt = "Please Enter where do you live in? (Answer in Pref Number)> "))
+
+if (input_data == input_data) equivalent = 0 else equivalent = 1
+
+#"Sex","Age","pref_u15","pref_now","equivalent"
+names(input_data) = c("sex", "age", "pref_u15", "pref_now", "equivalent")
 
